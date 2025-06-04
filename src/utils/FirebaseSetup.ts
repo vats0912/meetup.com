32faaaa3f71feb26266ce18f06edac1cbc07cc52
vats0912 <vatstanuj0904@gmail.com>
2025-06-04 22:18:01 +0530

    updated firebase file

--- src/utils/FirebaseSetup.ts
+++ src/utils/FirebaseSetup.ts
@@ -3,14 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth'
 import {collection,getFirestore} from 'firebase/firestore'
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCt_xvP4fzcYSRaGP69jI6JbVKlE-7pbwQ",
-  authDomain: "meetup-com.firebaseapp.com",
-  projectId: "meetup-com",
-  storageBucket: "meetup-com.firebasestorage.app",
-  messagingSenderId: "146264501170",
-  appId: "1:146264501170:web:476d004a9145437b4a12ab"
-};
+
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
