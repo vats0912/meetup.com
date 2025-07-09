15624261e4c975825b496ee475b26c6a9eadf84b
Tanuj vats <130696095+vats0912@users.noreply.github.com>
2025-07-09 17:36:17 +0530

    Update FirebaseSetup.ts

--- src/utils/FirebaseSetup.ts
+++ src/utils/FirebaseSetup.ts
@@ -8,7 +8,12 @@ import {collection,getFirestore} from 'firebase/firestore'
 // Initialize Firebase
 
 const firebaseConfig = {
-// firebase project keys
+  apiKey: process.env.API_KEY
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId:  process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId:  process.env.APP_ID
 };
 
 export const app = initializeApp(firebaseConfig);
